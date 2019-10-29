00bf791197642cefd3b7de3ab6f1f14cfad225c8
admin <lijiahui@lijiahuideMacBook-Air.local>
2019-10-29 09:41:11 +0800

    Mysql Project

---
+++ Go_Mysql/src/main/Mysql_Text.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+)
+import _ "github.com/go-sql-driver/mysql"
+
+func main() {
+	db, _ := sql.Open("mysql", "root:LJH787807080886@/springt")
+	result, _ := db.Exec(
+    "INSERT INTO user (name, password) VALUES (?, ?)",
+    "gopher",
+    "sasd",
+	)
+	fmt.Println(result)
+}
